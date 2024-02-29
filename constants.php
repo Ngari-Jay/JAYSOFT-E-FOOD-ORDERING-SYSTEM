7b319ea140eff086e185786db0f7401900be232f
NGARI JACKSON <116723897+Ngari-Jay@users.noreply.github.com>
2024-02-29 17:54:16 +0300

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,41 @@
+
+<!-----------/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
+Designed by JACKSON NGARI YAA an ICT Expert From Malindi 
+alSO dOUBLED to be ManAGING Director Of Jaysoft Systems
+which is a Technology Company owned by HIm*/
+/* Rich him through Email: jacksonngari93@gmail.com
+Company Email: jaysoftsystems93@gmail.com
+Secondary Email: systemsjaysoft@gmail.com
+Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/----->
+<?php 
+// lets start session
+session_start();
+// create constants to store non REPEATING VALUES
+define('SITEURL', 'http://localhost/Jaysoft%20E-Food/');
+define('LOCALHOST','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','food-order');
+/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
+Designed by JACKSON NGARI YAA an ICT Expert From Malindi 
+alSO dOUBLED to be ManAGING Director Of Jaysoft Systems
+which is a Technology Company owned by HIm*/
+/* Rich him through Email: jacksonngari93@gmail.com
+Company Email: jaysoftsystems93@gmail.com
+Secondary Email: systemsjaysoft@gmail.com
+Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/
+//3. database connection
+$conn = mysqli_connect(LOCALHOST,DB_USERNAME, DB_PASSWORD, DB_NAME);
+if ($conn == false) 
+{ 
+    echo "Database connection failed";
+}
+?>
+<!-----------/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
+Designed by JACKSON NGARI YAA an ICT Expert From Malindi 
+alSO dOUBLED to be ManAGING Director Of Jaysoft Systems
+which is a Technology Company owned by HIm*/
+/* Rich him through Email: jacksonngari93@gmail.com
+Company Email: jaysoftsystems93@gmail.com
+Secondary Email: systemsjaysoft@gmail.com
+Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/----->
