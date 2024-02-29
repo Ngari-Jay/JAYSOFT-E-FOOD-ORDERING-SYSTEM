af935e80830e3a950e7514f25ea7b3efbf0435c0
NGARI JACKSON <116723897+Ngari-Jay@users.noreply.github.com>
2024-02-29 17:56:05 +0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,23 +1,22 @@
-            
-<!-----------/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
-Designed by JACKSON NGARI YAA an ICT Expert From Malindi 
-alSO dOUBLED to be ManAGING Director Of Jaysoft Systems
-which is a Technology Company owned by HIm*/
-/* Rich him through Email: jacksonngari93@gmail.com
-Company Email: jaysoftsystems93@gmail.com
-Secondary Email: systemsjaysoft@gmail.com
-Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/----->
-        
-        
-        <!-------Footer section starts here--------->
-        <div class="footer">
-            <div class="wrapper">
-        <p class= "text-center"> 2024 All rights are reserved, Jaysoft Hotel Management, Designed By: <a href="#">Jaysoft Systems</a></p>
-        </div>
+<!-- social Section Starts Here -->
+<section class="social">
+    <div class="container text-center">
+        <ul>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+            </li>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+            </li>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+            </li>
+        </ul>
     </div>
-    <!-------Footer section Ends  here--------->
-    
-<!-----------/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
+</section>
+<!-- social Section Ends Here -->
+
+      <!-----------/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
 Designed by JACKSON NGARI YAA an ICT Expert From Malindi 
 alSO dOUBLED to be ManAGING Director Of Jaysoft Systems
 which is a Technology Company owned by HIm*/
@@ -25,5 +24,19 @@ which is a Technology Company owned by HIm*/
 Company Email: jaysoftsystems93@gmail.com
 Secondary Email: systemsjaysoft@gmail.com
 Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/----->
+
+
+<!-- footer Section Starts Here -->
+<section class="footer">
+    <div class="container text-center">
+    <div class="footer__social">
+                <a href="#" class="footer__icon"><i class='bx bxl-facebook' ></i></a>
+                <a href="#" class="footer__icon"><i class='bx bxl-instagram' ></i></a>
+                <a href="#" class="footer__icon"><i class='bx bxl-twitter' ></i></a>
+            </div>
+        <p>All rights reserved. Designed By <a href="#">&#169;Jaysoft Systems</a></p>
+    </div>
+</section>
+<!-- footer Section Ends Here -->
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -6,81 +6,50 @@ which is a Technology Company owned by HIm*/
 Company Email: jaysoftsystems93@gmail.com
 Secondary Email: systemsjaysoft@gmail.com
 Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/----->
-<?php 
-include('../config/constants.php')?>
-<?php
-include('partials/login-check.php') 
-?>
-<!-----------/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
-Designed by JACKSON NGARI YAA an ICT Expert From Malindi 
-alSO dOUBLED to be ManAGING Director Of Jaysoft Systems
-which is a Technology Company owned by HIm*/
-/* Rich him through Email: jacksonngari93@gmail.com
-Company Email: jaysoftsystems93@gmail.com
-Secondary Email: systemsjaysoft@gmail.com
-Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/----->
-    <!-------menu section starts here--------->
-
+<?php include('config/constants.php') ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jaysoft E-Food Ordering System | Homepage</title>
-    <link rel="stylesheet" href="../css/admin.css">
-    <style>
-    /*---------------css for the logo----------*/
-    .logo{
-        padding-right:250%
-       
-        }
-        .logo img{
-            height: 100px;
-            width:180px;
-        }
-        .text{
-            display: flex;
-            align:right;
-            color:green;
-        }
-    </style>
+    <title>Jaysoft E-Food Ordering System | HomePage</title>
+
+    <!-- lets Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-        <!-----------/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
-Designed by JACKSON NGARI YAA an ICT Expert From Malindi 
-alSO dOUBLED to be ManAGING Director Of Jaysoft Systems
-which is a Technology Company owned by HIm*/
-/* Rich him through Email: jacksonngari93@gmail.com
-Company Email: jaysoftsystems93@gmail.com
-Secondary Email: systemsjaysoft@gmail.com
-Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/----->
-    <!-------menu section starts here--------->
-    <div class="menu">
-        <div class="wrapper">
-       <ul>
-       <div class="logo"><img src="../images/JAYSOFT C LOGO.png" alt="">
-       </div>
-       <div class="text"><marquee behavior="left" direction=""><h1>Welcome To Jaysoft E-Food Ordering Main - Admin Panel</h1></marquee>
-   
-    </div>
-
-        <li><a href="index.php">Home</a></li>
-        <li><a href="manage-Admin.php">Admin</a></li>
-        <li><a href="manage-category">Category</a></li>
-        <li><a href="manage-food.php">Food</a></li>
-        <li><a href="manage-order.php">Order</a></li>
-        <li><a href="logout.php">Logout</a></li>
-       </ul>
-       </div>
+ <!-- Navigation bar Section Starts Here -->
+ <section class="navbar">
+    <div class="container">
+        <div class="logo">
+            <a href="index.php" title="Logo">
+                 <!-- logo insertion here -->
+                <img src="images/JAYSOFT C LOGO.png" alt="Restaurant Logo" class="img-responsive">
+            </a>
         </div>
+         <!-- menu bar section-->
+         <div class="menu text-right">
+            <ul>
+                <li>
+                    <a href="<?php echo SITEURL; ?>">Home</a>
+                </li>
+                <li>
+                    <a href="<?php echo SITEURL; ?>categories.php">Categories</a>
+                </li>
+                <li>
+                    <a href="<?php echo SITEURL; ?>foods.php">Foods</a>
+                </li>
+                <li>
+                    <a href="#">About Us</a>
+                </li>
+                <li>
+                    <a href="<?php echo SITEURL; ?>contact.php">Contact_Us</a>
+                </li>
+            </ul>
+        </div>
+        <div class="clearfix"></div>
     </div>
-    <!-------menu section Ends  here--------->
-
- <!-----------/*Welcome to JAYSOFT ONLINE FOOD ORDERING SYSTEM
-Designed by JACKSON NGARI YAA an ICT Expert From Malindi 
-alSO dOUBLED to be ManAGING Director Of Jaysoft Systems
-which is a Technology Company owned by HIm*/
-/* Rich him through Email: jacksonngari93@gmail.com
-Company Email: jaysoftsystems93@gmail.com
-Secondary Email: systemsjaysoft@gmail.com
-Call or Whatsapp: +254702134979 AVAILABLE AT ALL TIMES*/----->
-    <!-------menu section starts here--------->
+</section>
+<!-- Navbar Section Ends Here -->
